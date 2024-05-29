811bcc648c1e26b9c3267af586988a724b4c8cc8
enzosilva44 <110210457+enzosilva44@users.noreply.github.com>
2024-05-28 23:50:00 -0300

    CORS3.0

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,16 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-app.options('*', cors()); // Responder a todas as solicitações OPTIONS com os cabeçalhos CORS
+app.options('*', cors()); // Permitir solicitações OPTIONS de qualquer origem
+
+// Configurar cabeçalhos CORS para permitir solicitações da origem localhost:3000
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 const userRoutes = require('./routes/userRoutes');
 const authRoutes = require('./routes/authRoutes');
