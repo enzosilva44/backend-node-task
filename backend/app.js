13a737c85af60209cf4964bbc17dd3cac0dba842
enzosilva44 <110210457+enzosilva44@users.noreply.github.com>
2024-05-28 23:34:47 -0300

    CORS

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,11 @@ app.use(express.json()); // Middleware para processar o corpo das requisições
 app.use('/api/users', userRoutes); // Use as rotas de usuário definidas em userRoutes
 app.use('/api/auth', authRoutes); // Use as rotas de autenticação definidas em authRoutes
 
+// Definição das rotas
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
