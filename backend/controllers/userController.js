6aa4c3175b337d3aa7c8359ac5faa6e379388903
enzosilva44 <110210457+enzosilva44@users.noreply.github.com>
2024-06-07 10:14:50 -0300

    commit teste

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,14 +1,8 @@
-const User = require("../classes/userClass");
 const { insertUser, selectOneUserModel } = require("../models/userModel");
 
-module.exports = class CadastrosUsuarios {
-  static async postUser(req, res) {
+exports.postUser = async (req, res) => {
     try {
       const { nm_usuario, email, senha, } = req.body;
-
-      if (!nm_usuario || !email || !senha) {
-        return res.status(400).json({ error: true, message: "Dados incompletos" });
-      }
 
       const user = new User();
       user.nmUsuario = nm_usuario;
@@ -31,30 +25,3 @@ module.exports = class CadastrosUsuarios {
     }
   }
 
-  static async getUserById(req, res) {
-    try {
-      const id_user = req.params.idUser;
-
-      const user = new User();
-      user.idUser = id_user;
-
-      if (!user.idUser) {
-        return res.status(401).json({ error: true, message: "Dados incorretos" });
-      }
-
-      const result = await selectOneUserModel(user.convertToMapUser());
-
-      if (result.length === 0) {
-        return res.sendStatus(204);
-      }
-
-      return res.status(200).json({
-        error: false,
-        message: "Busca de dados com sucesso",
-        data: result,
-      });
-    } catch (err) {
-      return res.sendStatus(501);
-    }
-  }
-};
