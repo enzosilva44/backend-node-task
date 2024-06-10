2b4519e747c9cd68c15fe80d437243fa6f56fcd4
enzosilva44 <110210457+enzosilva44@users.noreply.github.com>
2024-06-10 15:17:21 -0300

    post task

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -3,11 +3,11 @@ const express = require('express');
 const router = express.Router();
 const taskController = require ('../controllers/taskController');
 
-router.get('/', taskController.getAllUsers);
+//router.get('/', taskController.getAllUsers);
 router.post('/register-task', taskController.postTask);
 
 
-router.put('/edit-user', taskController.editUser);
-router.delete('/delete-user', taskController.deleteUser);
+//router.put('/edit-user', taskController.editUser);
+//router.delete('/delete-user', taskController.deleteUser);
 
 module.exports = router;
